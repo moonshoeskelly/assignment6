c0fbf21489a792fa32805be6d38e05e150a21ce1
Kelly DeMent <kdement@ufl.edu>
2018-12-02 09:29:37 -0500

    add js, array, object, while loop

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+var members = [
+'Dave Grohl',
+'Nate Mendel',
+'Pat Smear',
+'Taylor Hawkins',
+'Rami Jaffee',
+'Chris Shiflett'
+ ];
+
+ var tour = {
+Chicago: "July 29th & 30th, 2018",
+Seattle: "September 1st, 2018",
+Edmonton: "September 4th, 2018",
+};
+
+var i = 1;
+
+while (i < 6) {
+  console.log('Times running through the loop ' + i);
+
+  i++;
+}
